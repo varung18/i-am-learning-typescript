4dd39cad4eacc92ceac16cadf2991abdf66fd3e9
Varun Gaur <95574483+varung18@users.noreply.github.com>
2025-07-19 19:03:37 +0530

    Add files via upload

---
+++ Shopping_Web_Page/src/hook/useCart.tsx
@@ -0,0 +1,9 @@
+import { useContext } from "react";
+import CartContext from "../context/CartProvider";
+import {useCartContextType} from "../context/CartProvider"
+
+const useCart = (): useCartContextType => {
+    return useContext(CartContext)
+}
+
+export default useCart
